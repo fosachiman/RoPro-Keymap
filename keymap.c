0d46b9ed5648dee1e64781d418d723bedab1c42e
Jason Humphreys <jasonphumphreys@gmail.com>
2024-07-13 01:00:00 -0400

    Update keymap.c
    moving tab to the bottom, bc tab switch not working anymore

--- keymap.c
+++ keymap.c
@@ -66,7 +66,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+-------------+------+------+------+------+------|
    * | Lower| Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
    * |------+------+------+------+------+------+------+------+------+------+------+------+------|
-   * | PgDn | Del  | Ctrl | Alt  | Cmd  |Lower |SHIFT |SPACE | Raise| Left | Down |  Up  |Right |
+   * | PgDn | Del  | Ctrl | Alt  | Cmd  |Lower | TAB  |SPACE | Raise| Left | Down |  Up  |Right |
    * `------------------------------------------------------------------------------------------'
    */
   	[_BASE] = LAYOUT(
@@ -75,7 +75,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC__MUTE, KC_ESC,    KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,      KC_P,     KC_BSPC,   
     KC_PGUP,  KC_TAB,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,      KC_SCLN,  KC_QUOT,
     KC_HOME,  KC_LSFT,   KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,    KC_SLSH,   KC_ENT,
-    KC_PGDN,  KC_DEL,    KC_RCTRL, KC_LALT,  KC_LCMD,  LOWER,    KC_RSFT,   KC_SPC,   RAISE,   KC_LEFT,  KC_DOWN,   KC_UP,    KC_RGHT
+    KC_PGDN,  KC_DEL,    KC_RCTRL, KC_LALT,  KC_LCMD,  LOWER,    KC_TAB,   KC_SPC,   RAISE,   KC_LEFT,  KC_DOWN,   KC_UP,    KC_RGHT
   ),
 
   /* LOWER
